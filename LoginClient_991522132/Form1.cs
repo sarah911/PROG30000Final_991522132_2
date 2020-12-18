10f018a71e272cb17e0080f40727ff335dc809a5
Suzi Ryu <ryusu@ryusu@sheridancollege.ca>
2020-12-18 16:08:58 -0500

    Add project files.

---
+++ LoginClient_991522132/Form1.cs
@@ -0,0 +1,38 @@
+﻿using LoginClient_991522132.TourService;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginClient_991522132
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TourService.AuthenticateUserServiceClient client = new TourService.AuthenticateUserServiceClient("NetTcpBinding_IAuthenticateUserService");
+            User user = new User();
+            user.UserName = textBox1.Text;
+            user.Password = textBox2.Text;
+
+            label3.Text = client.Login(user);
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
